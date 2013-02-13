825e0108cc20b1d70a54f018ccaaa16557b96a61
Matt Weir <mattweir10@gmail.com>
2013-02-13 01:13:32 -0600

    Use protocol-unspecific links to dependencies

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,8 +5,8 @@ requirejs.config({
 		}
 	},
 	paths: {
-		jquery: 'http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min',
-		easeljs: 'http://code.createjs.com/easeljs-0.5.0.min'
+		jquery: '//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min',
+		easeljs: '//code.createjs.com/easeljs-0.5.0.min'
 	}
 });
 

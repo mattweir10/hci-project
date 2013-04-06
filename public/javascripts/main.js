91e38e9fda7829d95fdc3d37804699b7ec863edd
Matt Weir <mattweir10@gmail.com>
2013-04-06 04:15:45 -0500

    Update to EaselJS 0.6.0

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -6,7 +6,7 @@ requirejs.config({
   },
   paths: {
     jquery: '//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min',
-    easeljs: '//code.createjs.com/easeljs-0.5.0.min'
+    easeljs: '//code.createjs.com/easeljs-0.6.0.min'
   }
 });
 

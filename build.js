8f7a744d064a812cf50f604446452ada6c7d6a23
Matt Weir <mattweir10@gmail.com>
2013-04-23 00:10:05 -0500

    Update build script

--- build.js
+++ build.js
@@ -1,6 +1,6 @@
 ({
-	baseUrl: "client",
-	mainConfigFile: "client/main.js",
+	baseUrl: "public/javascripts",
+	mainConfigFile: "public/javascripts/main.js",
 	name: "main",
 	out: "public/javascripts/index.js"
 })
